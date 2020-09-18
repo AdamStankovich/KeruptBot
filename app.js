829d11851b5756954544161cd2cc592283a8de81
jpasqui <jeremypasquino@gmail.com>
2020-09-18 17:17:00 -0400

    fixed newmaps and follow commands

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
+
 // Import Libraries
 const Banchojs = require("bancho.js");
-const fetch = require("node-fetch");
 
 // Personal info file (dont include)
 const { CLIENTID, CLIENTSECRET, USERNAME, PASSWORD } = require("./secret");
@@ -23,41 +23,7 @@ var followdict = new Object();
 // Read from file and populate dictionary
 // TODO
 
-// Retrieve access_token
-async function getAccessTokenPromise() {
-	const res = await fetch("https://osu.ppy.sh/oauth/token", {
-		method: "post",
-		headers: {
-			Accept: "application/json",
-			"Content-Type": "application/json",
-		},
-		body: JSON.stringify({
-			grant_type: "client_credentials",
-			// Insert own clientid/secret
-			client_id: CLIENTID,
-			client_secret: CLIENTSECRET,
-			scope: "public",
-		}),
-	});
-	return res.json();
-}
 
-// Retrieve beatmaps from mapper
-async function getBeatmaps(user, type, key) {
-	const res = await fetch(
-		`https://osu.ppy.sh/api/v2/users/${user}/beatmapsets/${type}`,
-		{
-			method: "get",
-			headers: {
-				// Authenticate OAuth2 using access_token ($key)
-				Authorization: `Bearer ${key}`,
-				Accept: "application/json",
-				"Content-Type": "application/json",
-			},
-		}
-	);
-	return res.json();
-}
 
 // The actual bot function
 const startOsuBot = async () => {
@@ -84,10 +50,10 @@ const startOsuBot = async () => {
 					await commands.newmaps(user, followdict);
 					break;
 				case prefix + "follow":
-					await commands.follow(user, followdict, message);
+					followdict = await commands.follow(user, followdict, message);
 					break;
 				case prefix + "unfollow":
-					await commands.unfollow(user, followdict, message);
+					followdict = await commands.unfollow(user, followdict, message);
 					break;
 			}
 		});
--- commands.js
+++ commands.js
@@ -1,3 +1,60 @@
+
+// Import Libraries
+const fetch = require("node-fetch");
+
+// Personal info file (dont include)
+const { CLIENTID, CLIENTSECRET, USERNAME, PASSWORD } = require("./secret");
+
+
+// Retrieve access_token
+async function getAccessTokenPromise() {
+	const res = await fetch("https://osu.ppy.sh/oauth/token", {
+		method: "post",
+		headers: {
+			Accept: "application/json",
+			"Content-Type": "application/json",
+		},
+		body: JSON.stringify({
+			grant_type: "client_credentials",
+			// Insert own clientid/secret
+			client_id: CLIENTID,
+			client_secret: CLIENTSECRET,
+			scope: "public",
+		}),
+	});
+	return res.json();
+}
+
+// Retrieve beatmaps from mapper
+async function getBeatmaps(user, type, key) {
+	const res = await fetch(
+		`https://osu.ppy.sh/api/v2/users/${user}/beatmapsets/${type}`,
+		{
+			method: "get",
+			headers: {
+				// Authenticate OAuth2 using access_token ($key)
+				Authorization: `Bearer ${key}`,
+				Accept: "application/json",
+				"Content-Type": "application/json",
+			},
+		}
+	);
+	return res.json();
+}
+
+function secondsToMinutes(seconds) {
+	newseconds = Math.floor(seconds % 60)
+	minutes = Math.floor(seconds / 60)
+	if (newseconds < 10) {
+		return `${minutes}:0${newseconds}`;
+	}
+	else {
+		return `${minutes}:${newseconds}`;
+	}
+}
+
+
+
 // List commands for the user
 async function help(user) {
 	return await user.sendMessage(
@@ -12,7 +69,7 @@ async function hello(user) {
 
 // Gets the user new maps
 async function newmaps(user, followdict) {
-	if (followdict[user.ircusername] !== undefined) {
+	if (followdict[user.ircUsername] !== undefined) {
 		var json = await getAccessTokenPromise();
 		var key = json.access_token;
 		for (var i = 0; i < followdict[user.ircUsername].length; i++) {
@@ -23,7 +80,7 @@ async function newmaps(user, followdict) {
 			);
 			for (var j = 0; j < json.length; j++) {
 				await user.sendMessage(
-					`https://osu.ppy.sh/beatmapsets/${json[j].id}/`
+					`[https://osu.ppy.sh/b/${json[j].beatmaps[0].id} ${json[j].artist} - ${json[j].title}] | Length: ${secondsToMinutes(json[j].beatmaps[0].total_length)} ⌛ | BPM: ${json[j].beatmaps[0].bpm} ♪ | AR: ${json[j].beatmaps[0].ar} ☉`
 				);
 			}
 		}
@@ -40,19 +97,20 @@ async function follow(user, followdict, message) {
 	if (followdict[user.ircUsername]) {
 		// If the user already follows the mapper, return
 		if (followdict[user.ircUsername].includes(userid)) {
-			return await user.sendMessage(
-				`You already follow ${userid} dumbass.`
-			);
+			await user.sendMessage(`You already follow ${userid}, dumbass.`);
+			return followdict;
 		}
 		// Append the mapper to the dictionary
 		followdict[user.ircUsername].push(userid);
-		return await user.sendMessage(`You followed ${userid}.`);
+		await user.sendMessage(`You followed ${userid}.`);
+		return followdict;
 	}
 	// If the user who sent the message is NOT already in the dictionary
 	else {
 		// Create the new key in the dictionary with the mapper as the value in an array
 		followdict[user.ircUsername] = [userid];
-		return await user.sendMessage(`You followed ${userid}.`);
+		await user.sendMessage(`You followed ${userid}.`);
+		return followdict;
 	}
 }
 
@@ -62,9 +120,11 @@ async function unfollow(user, followdict, message) {
 	if (followdict[user.ircUsername].includes(userid)) {
 		unfollow = followdict[user.ircUsername].indexOf(userid);
 		followdict[user.ircUsername].splice(unfollow, 1);
-		return await user.sendMessage(`You unfollowed ${userid}`);
+		await user.sendMessage(`You unfollowed ${userid}`);
+		return followdict;
 	} else {
-		return await user.sendMessage(`You don't follow ${userid} dumbass.`);
+		await user.sendMessage(`You don't follow ${userid}, dumbass.`);
+		return followdict;
 	}
 }
 
